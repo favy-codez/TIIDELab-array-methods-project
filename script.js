d40b6032e473c81adb6522a81a075049c2d07222
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-09-11 12:40:00 +0100

    added html

---
+++ script.js
@@ -0,0 +1,31 @@
+let users = [{
+    firstName : "Godsfavour",
+    lastName : "Ezeliora",
+    email : "ezeliorafavour81@gmail.com",
+    profileImage : "/IMAGES/img.jpeg",
+},
+{
+    firstName : "Oluebube",
+    lastName : "Eze",
+    email : "ezeliorafavour81@gmail.com",
+    profileImage : "/IMAGES/img.jpeg",
+},
+{
+    firstName : "Favour",
+    lastName : "Madumere",
+    email : "ezeliorafavour81@gmail.com",
+    profileImage : "/IMAGES/img.jpeg",
+},
+{
+    firstName : "Nse",
+    lastName : "Abraham",
+    email : "ezeliorafavour81@gmail.com",
+    profileImage : "/IMAGES/me.JPG",
+},
+{
+    firstName : "Nonso",
+    lastName : "Ezeliora",
+    email : "ezeliorafavour81@gmail.com",
+    profileImage : "/IMAGES/img.jpeg",
+},
+];
