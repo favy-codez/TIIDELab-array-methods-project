6bbcc2ecdeb3288d54007e47a6eec942867976de
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-09-12 16:20:30 +0100

    added functionalities

--- script.js
+++ script.js
@@ -1,31 +1,56 @@
-let users = [{
+let users = [
+    {
     firstName : "Godsfavour",
     lastName : "Ezeliora",
     email : "ezeliorafavour81@gmail.com",
-    profileImage : "/IMAGES/img.jpeg",
+    profileImage : "images/img.jpg"
 },
 {
     firstName : "Oluebube",
     lastName : "Eze",
     email : "ezeliorafavour81@gmail.com",
-    profileImage : "/IMAGES/img.jpeg",
+    profileImage : "./images/img1.jpg"
 },
 {
     firstName : "Favour",
     lastName : "Madumere",
     email : "ezeliorafavour81@gmail.com",
-    profileImage : "/IMAGES/img.jpeg",
+    profileImage : "./images/img.jpg"
 },
 {
     firstName : "Nse",
     lastName : "Abraham",
     email : "ezeliorafavour81@gmail.com",
-    profileImage : "/IMAGES/me.JPG",
+    profileImage : "./images/img1.jpg"
 },
 {
     firstName : "Nonso",
     lastName : "Ezeliora",
     email : "ezeliorafavour81@gmail.com",
-    profileImage : "/IMAGES/img.jpeg",
+    profileImage : "./images/img.JPG"
+},
+{
+    firstName : "Obinna",
+    lastName : "Eze",
+    email : "ezeliorafavour81@gmail.com",
+    profileImage : "./images/img1.JPG"
 },
 ];
+
+let usersContainer = document.getElementById("users");
+let userBox = '';
+users.forEach((user) => {
+    userBox += ` <div class = "user">
+    <img src="${user.profileImage}" alt="${user.firstName}">
+    ${user.firstName} ${user.lastName}</div>
+    `
+    usersContainer.innerHTML = userBox;
+});
+
+function removeFirstUser(){
+
+};
+
+function removeLastUser(){
+
+};
